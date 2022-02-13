f399eb6c5dfbcd8740d4dd0f2859b95636a14c96
razumolla <rajuahmed9344@gmail.com>
2022-02-13 21:39:54 +0600

    update subtotal

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ function updateProductNumber(product, price, isIncreasing) {
     // update total money
     const productTotal = document.getElementById(product + '-total');
     productTotal.innerText = productNumber * price;
+    // calculate subtotal money
+    calculateTotal();
 }
 
 // phone increase decrease events
@@ -29,3 +31,17 @@ document.getElementById('case-plus').addEventListener('click', function () {
 document.getElementById('case-minus').addEventListener('click', function () {
     updateProductNumber('case', 59, false);
 });
+
+// final calculation
+function getInputValue(product) {
+    const productInput = document.getElementById(product + '-number');
+    const productNumber = parseInt(productInput.value);
+    return productNumber;
+}
+function calculateTotal() {
+    const phoneTotal = getInputValue('phone') * 1219;
+    const caseTotal = getInputValue('case') * 59;
+    const subTotal = phoneTotal + caseTotal;
+    // update on the html
+    document.getElementById('sub-total').innerText = subTotal;
+}
