0fac84bbbb5e70ea44ecdd5486bcce431dfd4fa8
razumolla <rajuahmed9344@gmail.com>
2022-02-13 21:05:17 +0600

    update phone cost c-3

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
-
-function updateCaseNumber(isIncreasing) {
-    const caseInput = document.getElementById('case-number')
-    let caseNumber = caseInput.value;
+function updateProductNumber(product, price, isIncreasing) {
+    const productInput = document.getElementById(product + '-number')
+    let productNumber = productInput.value;
     if (isIncreasing) {
-        caseNumber = parseInt(caseNumber) + 1;
+        productNumber = parseInt(productNumber) + 1;
     }
-    else if (caseNumber > 0) {
-        caseNumber = parseInt(caseNumber) - 1;
+    else if (productNumber > 0) {
+        productNumber = parseInt(productNumber) - 1;
     }
-    caseInput.value = caseNumber;
-    // update case total money
-    const caseTotal = document.getElementById('case-total');
-    caseTotal.innerText = caseNumber * 59;
+    productInput.value = productNumber;
+    // update total money
+    const productTotal = document.getElementById(product + '-total');
+    productTotal.innerText = productNumber * price;
 }
 
-
-document.getElementById('case-plus').addEventListener('click',
-    function () {
-        updateCaseNumber(true);
-    }
-)
-
-document.getElementById('case-minus').addEventListener('click',
-    function () {
-        updateCaseNumber(false);
-    }
-)
+// phone increase decrease events
+document.getElementById('phone-plus').addEventListener('click', function () {
+    updateProductNumber('phone', 1219, true);
+});
+document.getElementById('phone-minus').addEventListener('click', function () {
+    updateProductNumber('phone', 1219, false);
+});
 
 
-
-// console.log(caseNumber)
+// handle case increase decrease events
+document.getElementById('case-plus').addEventListener('click', function () {
+    updateProductNumber('case', 59, true);
+});
+document.getElementById('case-minus').addEventListener('click', function () {
+    updateProductNumber('case', 59, false);
+});
